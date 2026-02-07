c17fcceae477ca1ed10c4577cd441851ce56599b
x3r1x <3opobep@gmail.com>
2026-02-07 14:13:10 +0300

    lab1.1 accepted by Dmitry Rybakov

--- PM/lab1/lab1.1_fib.c
+++ PM/lab1/lab1.1_fib.c
@@ -4,21 +4,31 @@
 
 #include <stdio.h>
 
-int fib(const int n) {
+int fib(const int n)
+{
     int result;
 
-    if (n == 0) {
+    if (n == 0)
+    {
         result = 0;
-    } else if (n == 1) {
+    }
+    else if (n == 1)
+    {
         result = 1;
-    } else {
+    }
+    else
+    {
         int a = 0;
         int b = 1;
         result = a + b;
-        for (int i = 2; i < n; ++i) {
-            if (a <= b) {
+        for (int i = 2; i < n; ++i)
+        {
+            if (a <= b)
+            {
                 a = result;
-            } else {
+            }
+            else
+            {
                 b = result;
             }
 
@@ -29,17 +39,20 @@ int fib(const int n) {
     return result;
 }
 
-int main(void) {
+int main(void)
+{
     int n;
 
-    printf("Введите n-ое число Фибонначи, которое надо вывести! Для завершения программы введите EOF\n");
-    if (scanf("%d", &n) != 1) {
+    printf("Введите n-ое число Фибонначи, которое надо вывести!\n");
+    if (scanf("%d", &n) != 1)
+    {
         printf("Неверный формат ввода!\n");
         return 1;
     }
-    if (n < 0) {
+    if (n < 0)
+    {
         printf("Число %d меньше нуля, а значит такое число Фибонначи не существует!\n", n);
-        return 2;
+        return 1;
     }
     printf("%d\n", fib(n));
 }
