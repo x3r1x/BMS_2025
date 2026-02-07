4e9a12290222822effce5c8ee25d0eb9d27baac1
x3r1x <3opobep@gmail.com>
2026-02-07 21:30:09 +0300

    lab1.2 done

---
+++ PM/lab1/lab1.2_numbers.c
@@ -0,0 +1,52 @@
+//
+// Created by x3r1x on 07.02.2026.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    int min, max, even, odd, num, check, cnt, sum;
+
+    while ((check = scanf("%d", &num)) == 1 || getchar() != EOF)
+    {
+        if (check == 1)
+        {
+            if (cnt == 0)
+            {
+                min = num;
+                max = num;
+            }
+            else
+            {
+                if (min > num)
+                {
+                    min = num;
+                }
+
+                if (max < num)
+                {
+                    max = num;
+                }
+            }
+
+            if (num % 2 == 0)
+            {
+                even += 1;
+            }
+            else
+            {
+                odd += 1;
+            }
+
+            cnt += 1;
+            sum += num;
+        }
+    }
+
+    printf("\nCount: %d\nSum: %d\n", cnt, sum);
+    if (cnt != 0)
+    {
+        printf("Min: %d\nMax: %d\nEven: %d\nOdd: %d\n", min, max, even, odd);
+    }
+}
