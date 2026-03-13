d6c2a27212658b661c259a3153056681d2c2beb5
x3r1x <3opobep@gmail.com>
2026-03-14 00:52:38 +0300

    lab4.3.c extra1 done!

--- PM/labs/lab4/lab4.3.c
+++ PM/labs/lab4/lab4.3.c
@@ -19,8 +19,8 @@ typedef struct
 
 bool IsPointInputSuccess(Point** array, int len, bool* isMemoryError);
 bool IsPolygonConvex(Polygon polygon);
+bool IsSelfIntersect(Polygon polygon);
 void FreePointArray(Point** array, int len);
-void PrintPoints(Point** vertices, int len);
 
 int main(void)
 {
@@ -43,7 +43,11 @@ int main(void)
 	if (isOutOfMemory)
 		goto memoryError;
 
-	if (IsPolygonConvex(polygon))
+	if (IsSelfIntersect(polygon))
+	{
+		printf("Self-intersecting\n");
+	}
+	else if (IsPolygonConvex(polygon))
 	{
 		printf("Convex\n");
 	}
@@ -63,6 +67,73 @@ memoryError:
 
 	FreePointArray(polygon.vertices, polygon.verticesCount);
 	return 2;
+}
+
+int Orient(const Point* P, const Point* Q, const Point* R)
+{
+	return (Q->x - P->x) * (R->y - P->y) - (Q->y - P->y) * (R->x - P->x);
+}
+
+int Min(const int a, const int b)
+{
+	if (a <= b)
+		return a;
+
+	return b;
+}
+
+int Max(const int a, const int b)
+{
+	if (a >= b)
+		return a;
+
+	return b;
+}
+
+bool isPointInsideASegment(const Point* A, const Point* B, const Point* C, const int orientCtoAB)
+{
+	return orientCtoAB == 0
+	&& Min(A->x, B->x) <= C->x && C->x <= Max(A->x, B->x)
+	&& Min(A->y, B->y) <= C->y && C->y <= Max(A->y, B->y);
+}
+
+bool IsSelfIntersect(Polygon polygon)
+{
+	const int lastMainSideIndex = polygon.verticesCount / 2 + polygon.verticesCount % 2;
+
+	for (int i = 0; i < lastMainSideIndex; i++)
+	{
+		const Point* A = polygon.vertices[i];
+		const Point* B = polygon.vertices[i + 1];
+
+		int preventFallthroughRightSide = 0;
+		if (i == 0)
+			preventFallthroughRightSide = 1;
+
+		for (int j = i + 2; j < polygon.verticesCount - preventFallthroughRightSide; j++)
+		{
+			const Point* C = polygon.vertices[j];
+			const Point* D = polygon.vertices[(j + 1) % polygon.verticesCount];
+
+			const int orientCtoAB = Orient(A, B, C);
+			const int orientDtoAB = Orient(A, B, D);
+			const int orientAtoCD = Orient(C, D, A);
+			const int orientBtoCD = Orient(C, D, B);
+
+			if (orientCtoAB * orientDtoAB < 0 && orientBtoCD * orientAtoCD < 0)
+			{
+				return true;
+			}
+
+			if (isPointInsideASegment(A, B, C, orientCtoAB) || isPointInsideASegment(A, B, D, orientDtoAB)
+				|| isPointInsideASegment(C, D, A, orientAtoCD) || isPointInsideASegment(C, D, B, orientBtoCD))
+			{
+				return true;
+			}
+		}
+	}
+
+	return false;
 }
 
 void FreePointArray(Point** array, const int len)
@@ -108,7 +179,6 @@ bool IsPointInputSuccess(Point** array, const int len, bool* isMemoryError)
 
 bool IsPolygonConvex(const Polygon polygon)
 {
-	bool isConvex = true;
 	int cross = 0;
 
 	for (int i = 0; i < polygon.verticesCount; i++)
@@ -117,7 +187,7 @@ bool IsPolygonConvex(const Polygon polygon)
 		const Point* B = polygon.vertices[(i + 1) % polygon.verticesCount];
 		const Point* C = polygon.vertices[(i + 2) % polygon.verticesCount];
 
-		const int currentCross = (B->x - A->x) * (C->y - B->y) - (B->y - A->y) * (C->x - B->x);
+		const int currentCross = Orient(A, B, C);
 
 		if (currentCross != 0)
 		{
@@ -127,21 +197,11 @@ bool IsPolygonConvex(const Polygon polygon)
 			}
 			else
 			{
-				isConvex = cross * currentCross > 0;
-
-				if (!isConvex)
-					break;
+				if (cross * currentCross < 0)
+					return false;
 			}
 		}
 	}
 
-	return isConvex;
-}
-
-void PrintPoints(Point** vertices, const int len)
-{
-	for (int i = 0; i < len; i++)
-	{
-		printf("Point %d: (%d; %d)\n", i + 1, vertices[i]->x, vertices[i]->y);
-	}
+	return true;
 }
