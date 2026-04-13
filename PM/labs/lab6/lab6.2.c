3d5f35a01c207ce85b087c246eed2f9bac50688b
x3r1x <3opobep@gmail.com>
2026-04-13 15:34:23 +0300

    lab6.2.c accepted

---
+++ PM/labs/lab6/lab6.2.c
@@ -0,0 +1,142 @@
+//
+// Created by x3r1x on 11.04.2026.
+//
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+	double x;
+	double y;
+} Coordinates;
+
+typedef struct
+{
+	Coordinates coordinates;
+	double radius;
+} Island;
+
+typedef struct
+{
+	double A;
+	double B;
+	double C;
+} SquareEquationCoefficients;
+
+bool IsInputReadSuccessful(Coordinates* startDot, Coordinates* direction, int* islandsCount);
+Island* ReadIslandsArray(int islandsCount);
+int GetClosestIslandIndex(Coordinates startPos, Coordinates direction, const Island* islands, int islandsCount);
+
+int main()
+{
+	Coordinates startDot, direction;
+	Island* islands;
+	int islandsCount;
+
+	if (!IsInputReadSuccessful(&startDot, &direction, &islandsCount))
+	{
+		printf("Error: wrong input!\n");
+		return 1;
+	}
+
+	islands = ReadIslandsArray(islandsCount);
+
+	if (islands == nullptr)
+	{
+		printf("Error!\n");
+		return 1;
+	}
+
+	printf("%d", GetClosestIslandIndex(startDot, direction, islands, islandsCount));
+
+	free(islands);
+}
+
+bool IsInputReadSuccessful(Coordinates* startDot, Coordinates* direction, int* islandsCount)
+{
+	if (scanf("%lf %lf", &startDot->x, &startDot->y) != 2)
+	{
+		return false;
+	}
+
+	if (scanf("%lf %lf", &direction->x, &direction->y) != 2)
+	{
+		return false;
+	}
+
+	if (scanf("%d", islandsCount) != 1)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+Island* ReadIslandsArray(const int islandsCount)
+{
+	Island* islandsArray = malloc(islandsCount * sizeof(Island));
+
+	if (islandsArray == nullptr)
+	{
+		return nullptr;
+	}
+
+	for (int i = 0; i < islandsCount; i++)
+	{
+		if (scanf("%lf %lf %lf", &islandsArray[i].coordinates.x, &islandsArray[i].coordinates.y, &islandsArray[i].radius) != 3)
+		{
+			free(islandsArray);
+			return nullptr;
+		}
+	}
+
+	return islandsArray;
+}
+
+SquareEquationCoefficients GetCoefficients(const Coordinates startDot, const Coordinates direction, const Island island)
+{
+	SquareEquationCoefficients coefficients;
+
+	coefficients.A = pow(direction.x, 2) + pow(direction.y, 2);
+	coefficients.B = 2 * (direction.x * (startDot.x - island.coordinates.x) + direction.y * (startDot.y - island.coordinates.y));
+	coefficients.C = pow(startDot.x - island.coordinates.x, 2) + pow(startDot.y - island.coordinates.y, 2) - pow(island.radius, 2);
+
+	return coefficients;
+}
+
+double GetDiscriminant(const SquareEquationCoefficients coefficients)
+{
+	return pow(coefficients.B, 2) - 4 * coefficients.C * coefficients.A;
+}
+
+double GetMinTime(const SquareEquationCoefficients coefficients, const double discriminant)
+{
+	return (-coefficients.B - sqrt(discriminant)) / (2 * coefficients.A);
+}
+
+int GetClosestIslandIndex(const Coordinates startPos, const Coordinates direction, const Island* islands, const int islandsCount)
+{
+	int minIslandIndex = -1;
+	double minDiscriminant = 0;
+
+	for (int i = 0; i < islandsCount; i++)
+	{
+		const SquareEquationCoefficients coefficients = GetCoefficients(startPos, direction, islands[i]);
+		const double discriminant = GetDiscriminant(coefficients);
+
+		if (discriminant >= 0 && (minIslandIndex == -1 || discriminant < minDiscriminant))
+		{
+			const double minTime = GetMinTime(coefficients, discriminant);
+
+			if (minTime >= 0)
+			{
+				minDiscriminant = discriminant;
+				minIslandIndex = i;
+			}
+		}
+	}
+
+	return minIslandIndex;
+}
